1d7e56bdd5f66b2864f47439c25dab97bbbcaa03
Anonymous <anonymous>
2023-12-13 17:55:43 +0200

    Solve faults tutorial

--- faults/handler.c
+++ faults/handler.c
@@ -48,9 +48,11 @@ int main(void)
         badged_faulter_fault_ep_cap,
         seL4_WordBits,
         handler_cspace_root,
-        /* EXERCISE: Which capability should be the source argument here? */-1,
+        faulter_fault_ep_cap,
         seL4_WordBits,
-        seL4_AllRights, FAULTER_BADGE_VALUE);
+        seL4_AllRights, 
+        FAULTER_BADGE_VALUE
+    );
 
     ZF_LOGF_IF(error != 0, PROGNAME "Failed to mint ep cap with badge!");
     printf(PROGNAME "Successfully minted fault handling ep into local cspace.\n");
@@ -61,36 +63,67 @@ int main(void)
      * the faulting thread's cspace on the MCS kernel.
      */
 
+    // put fault endpoint in faulter's cspace
     error = seL4_CNode_Copy(
         faulter_cspace_root,
-        /* EXERCISE: What should the destination be for this cap copy? */-1,
+        foreign_badged_faulter_empty_slot_cap,
         seL4_WordBits,
         handler_cspace_root,
         badged_faulter_fault_ep_cap,
         seL4_WordBits,
-        seL4_AllRights);
+        seL4_AllRights
+    );
 
     ZF_LOGF_IF(error != 0, PROGNAME "Failed to copy badged ep cap into faulter's cspace!");
     printf(PROGNAME "Successfully copied badged fault handling ep into "
            "faulter's cspace.\n"
            PROGNAME "(Only necessary on Master kernel.)\n");
 
+    // set faulter TCB fault endpoint
+    error = seL4_TCB_SetSpace(
+        faulter_tcb_cap,
+        foreign_badged_faulter_empty_slot_cap,
+        faulter_cspace_root,
+        0,
+        faulter_vspace_root,
+        0
+    );
+    ZF_LOGF_IF(error != 0, PROGNAME "Failed to set TCB fault endpoint!");
+    printf(PROGNAME "Successfully set faulter's TCB fault endpoint\n");
+
+    // resume faulter
+    seL4_Reply(seL4_MessageInfo_new(0, 0, 0, 0));
+    printf(PROGNAME "Resumed faulter\n");
+
+    // await a fault
+    seq_msginfo = seL4_Recv(faulter_fault_ep_cap, &tmp_badge);
+    printf(PROGNAME "Received a fault\n");
+
+    // ASSUMPTION - this was a capability fault which was caused by accessing an empty cap
+
+    // get the empty cap slot
+    foreign_faulter_capfault_cap = seL4_GetMR(seL4_CapFault_Addr);
+    printf(PROGNAME "Got faulty cap slot: %p\n", foreign_faulter_capfault_cap);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // put the endpoint cap into the faulty slot 
+    // (must be an endpoint cap because seL4_NBRecv is used on it)
+    // such that when the faulter retries it will succeed recieving
+    error = seL4_CNode_Copy(
+        faulter_cspace_root,
+        foreign_faulter_capfault_cap,
+        seL4_WordBits,
+        handler_cspace_root,
+        sequencing_ep_cap,
+        seL4_WordBits,
+        seL4_AllRights
+    );
+    ZF_LOGF_IF(error != 0, PROGNAME "Failed to copy ep cap into faulter's faulty cap slot!");
+    printf(PROGNAME "Copied endpoint into faulter's faulty cap access slot\n");
+    
+    // wake up the faulter (causing it to retry cap access) by replying to the fault endpoint
+    seL4_Reply(seL4_MessageInfo_new(0, 0, 0, 0));
+    printf(PROGNAME "Resumed faulter\n");
 
     return 0;
 }
